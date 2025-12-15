78c48a433ba4fe6ba8815e7a540e77def7d8950c
MbaksKwatu <mbakayaweever@gmail.com>
2025-12-15 10:34:15 +0300

    Fix Next.js App Router config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../')
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
